377d4b6bffdb55fa06df3944376ae99896dc750b
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-05-06 10:07:03 +0200

    add play again

--- main.c
+++ main.c
@@ -72,97 +72,102 @@ int main() {
     }
 
     srand((unsigned int)time(NULL));
-    char *secret = words[rand() % word_count];
-    int secret_len = strlen(secret);
 
-    int letters[256] = {0};
-    int guessed[256] = {0};
-    char wrong[27] = {0};
-    int lives = 0;
+    int play = 1;
+    while (play) {
+        char *secret = words[rand() % word_count];
+        int secret_len = strlen(secret);
+        int letters[256] = {0};
+        int guessed[256] = {0};
+        char wrong[27] = {0};
+        int lives = 0;
 
-    for (int i = 0; i < secret_len; i++)
-        letters[(unsigned char)secret[i]] = 1;
+        for (int i = 0; i < secret_len; i++)
+            letters[(unsigned char)secret[i]] = 1;
 
-    while (1) {
-        clear_screen();
-        printf("===============\n");
-        printf(" phoenixthrush\n");
-        printf("===============\n");
+        while (1) {
+            clear_screen();
+            printf("===============\n");
+            printf(" phoenixthrush\n");
+            printf("===============\n");
 
-        if (lives == 10) {
-            printf("You lose!\n");
-            printf("It was: ");
-            for (int i = 0; i < secret_len; i++)
-                printf("%c", toupper((unsigned char)secret[i]));
-            printf("\n");
-            break;
-        } else {
-            printf("%s", HANGMAN[lives]);
-        }
+            if (lives == 10) {
+                printf("You lose!\n");
+                printf("It was: ");
+                for (int i = 0; i < secret_len; i++)
+                    printf("%c", toupper((unsigned char)secret[i]));
+                printf("\n");
+                break;
+            } else {
+                printf("%s", HANGMAN[lives]);
+            }
 
-        printf("===============\n\n");
-        printf("Word: ");
-        for (int i = 0; i < secret_len; i++) {
-            if (guessed[(unsigned char)secret[i]])
-                printf("%c ", toupper((unsigned char)secret[i]));
-            else
-                printf("_ ");
-        }
-        printf("\n\n");
+            printf("===============\n\n");
+            printf("Word: ");
+            for (int i = 0; i < secret_len; i++) {
+                if (guessed[(unsigned char)secret[i]])
+                    printf("%c ", toupper((unsigned char)secret[i]));
+                else
+                    printf("_ ");
+            }
+            printf("\n\n");
 
-        int won = 1;
-        for (int i = 0; i < 256; i++) {
-            if (letters[i] && !guessed[i]) {
-                won = 0;
+            int won = 1;
+            for (int i = 0; i < 256; i++) {
+                if (letters[i] && !guessed[i]) {
+                    won = 0;
+                    break;
+                }
+            }
+            if (won) {
+                printf("You won!\n");
                 break;
             }
-        }
-        if (won) {
-            printf("You won!\n");
-            break;
-        }
 
-        printf("Lives: %d\n", 9 - lives);
-        if (wrong[0])
-            printf("Wrong: %s\n", wrong);
-        printf("\n");
-
-        if (lives == 9) {
-            printf("You lose!\n");
-            printf("It was: ");
-            for (int i = 0; i < secret_len; i++)
-                printf("%c", toupper((unsigned char)secret[i]));
+            printf("Lives: %d\n", 9 - lives);
+            if (wrong[0])
+                printf("Wrong: %s\n", wrong);
             printf("\n");
-            break;
-        }
 
-        printf("Make a guess: ");
-        char guess_input[100];
-        if (!fgets(guess_input, sizeof(guess_input), stdin) || guess_input[0] == '\n')
-            continue;
+            if (lives == 9) {
+                printf("You lose!\n");
+                printf("It was: ");
+                for (int i = 0; i < secret_len; i++)
+                    printf("%c", toupper((unsigned char)secret[i]));
+                printf("\n");
+                break;
+            }
 
-        char c = tolower((unsigned char)guess_input[0]);
+            printf("Make a guess: ");
+            char guess_input[100];
+            if (!fgets(guess_input, sizeof(guess_input), stdin) || guess_input[0] == '\n')
+                continue;
 
-        if (letters[(unsigned char)c]) {
-            guessed[(unsigned char)c] = 1;
-        } else {
-            if (!strchr(wrong, toupper((unsigned char)c))) {
-                size_t len = strlen(wrong);
-                if (len < 26) {
-                    wrong[len] = toupper((unsigned char)c);
-                    wrong[len + 1] = '\0';
-                    lives++;
+            char c = tolower((unsigned char)guess_input[0]);
+
+            if (letters[(unsigned char)c]) {
+                guessed[(unsigned char)c] = 1;
+            } else {
+                if (!strchr(wrong, toupper((unsigned char)c))) {
+                    size_t len = strlen(wrong);
+                    if (len < 26) {
+                        wrong[len] = toupper((unsigned char)c);
+                        wrong[len + 1] = '\0';
+                        lives++;
+                    }
                 }
             }
         }
+
+        printf("\nPlay again? (y/n): ");
+        char again_input[10];
+        if (!fgets(again_input, sizeof(again_input), stdin) || tolower(again_input[0]) != 'y')
+            play = 0;
     }
 
     for (int i = 0; i < word_count; i++)
         free(words[i]);
     free(words);
-
-    printf("\nPress Enter to exit...\n");
-    getchar();
 
     return 0;
 }
